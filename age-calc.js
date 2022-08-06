9c807007f4fbbd4130e2a5e879c53d25ef1d8f06
Omid Ghahramani <omid.12.ghari@gmail.com>
2022-08-06 03:39:06 +0000

    calculates age based on year of birth

---
+++ age-calc.js
@@ -0,0 +1,5 @@
+const ageCalculator = function (name, yearOfBirth, currentYear) {
+  var age = currentYear - yearOfBirth;
+  return name + " is " + age + " years old.";
+}
+console.log(ageCalculator("Miranda", 1983, 2015));
